0d9538e210223d2efdfb02ff2bb555f055e8bd68
Grigory Bordyugov <g.bordyugov@criteo.com>
2026-04-09 15:15:11 +0200

    working on MyMaybeT

--- src/Exercises/MyMaybeT.hs
+++ src/Exercises/MyMaybeT.hs
@@ -3,19 +3,12 @@ module Exercises.MyMaybeT where
 newtype MyMaybeT m a = MyMaybeT { runMyMaybeT :: m (Maybe a) }
 
 
-instance (Semigroup m) => Semigroup (MyMaybeT m a) where
-  (<>) = undefined
-
-
-instance (Monoid m) => Monoid (MyMaybeT m a) where
-  mempty = undefined
-
-
 instance (Functor m) => Functor (MyMaybeT m) where
-  fmap = undefined
-  
+  fmap f (MyMaybeT mma) = MyMaybeT $ undefined
+
 
 instance (Applicative m) => Applicative (MyMaybeT m) where
+  pure = undefined
   (<*>) = undefined
 
 
